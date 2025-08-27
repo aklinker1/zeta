83a40595161e55298f6020b9dd342e1db68745f6
Aaron <aaronklinker1@gmail.com>
2025-08-27 08:33:40 -0600

    chore: Add test for client input types

--- src/__tests__/client.test.ts
+++ src/__tests__/client.test.ts
@@ -4,63 +4,88 @@ import { createTestAppClient } from "../testing";
 import type { App } from "../types";
 import type { GetClientRoutes } from "../client";
 import { expectTypeOf } from "expect-type";
+import { z } from "zod/v4";
 
 describe("Client", () => {
-  describe("params", () => {
-    it("should apply named path params correctly", async () => {
-      const id = "one";
-      const expectedPath = `/api/users/${id}`;
-      const expectedParams = { id };
+  describe("inputs", () => {
+    describe("params", () => {
+      it("should apply named path params correctly", async () => {
+        const id = "one";
+        const expectedPath = `/api/users/${id}`;
+        const expectedParams = { id };
 
-      let actual: any;
-      const app = createApp().get(
+        let actual: any;
+        const app = createApp().get(
+          "/api/users/:id",
+          (ctx) => void (actual = ctx),
+        );
+        const client = createTestAppClient(app);
+
+        await client.fetch("GET", "/api/users/:id", {
+          params: { id },
+        });
+
+        expect(actual.path).toEqual(expectedPath);
+        expect(actual.params).toEqual(expectedParams);
+      });
+
+      it("should apply anonymous wildcard path params correctly", async () => {
+        const id = "one";
+        const expectedPath = `/api/users/${id}`;
+        const expectedParams = { "**": id };
+
+        let actual: any;
+        const app = createApp().get(
+          "/api/users/**",
+          (ctx) => void (actual = ctx),
+        );
+        await createTestAppClient(app).fetch("GET", "/api/users/**", {
+          params: { "**": id },
+        });
+
+        expect(actual.path).toEqual(expectedPath);
+        expect(actual.params).toEqual(expectedParams);
+      });
+
+      it("should apply named wildcard path params correctly", async () => {
+        const id = "one";
+        const expectedPath = `/api/users/${id}`;
+        const expectedParams = { id };
+
+        let actual: any;
+        const app = createApp().get(
+          "/api/users/**:id",
+          (ctx) => void (actual = ctx),
+        );
+        await createTestAppClient(app).fetch("GET", "/api/users/**:id", {
+          params: { id },
+        });
+
+        expect(actual.path).toEqual(expectedPath);
+        expect(actual.params).toEqual(expectedParams);
+      });
+    });
+
+    it("should require any params from the definition", async () => {
+      const app = createApp().put(
         "/api/users/:id",
-        (ctx) => void (actual = ctx),
+        {
+          operationId: "updateUser",
+          params: z.object({
+            id: z.string(),
+          }),
+          body: z.object({
+            name: z.string(),
+          }),
+        },
+        () => {},
       );
       const client = createTestAppClient(app);
 
-      await client.fetch("GET", "/api/users/:id", {
-        params: { id },
+      await client.fetch("PUT", "/api/users/:id", {
+        body: { name: "test" },
+        params: { id: "123" },
       });
-
-      expect(actual.path).toEqual(expectedPath);
-      expect(actual.params).toEqual(expectedParams);
-    });
-
-    it("should apply anonymous wildcard path params correctly", async () => {
-      const id = "one";
-      const expectedPath = `/api/users/${id}`;
-      const expectedParams = { "**": id };
-
-      let actual: any;
-      const app = createApp().get(
-        "/api/users/**",
-        (ctx) => void (actual = ctx),
-      );
-      await createTestAppClient(app).fetch("GET", "/api/users/**", {
-        params: { "**": id },
-      });
-
-      expect(actual.path).toEqual(expectedPath);
-      expect(actual.params).toEqual(expectedParams);
-    });
-
-    it("should apply named wildcard path params correctly", async () => {
-      const id = "one";
-      const expectedPath = `/api/users/${id}`;
-      const expectedParams = { id };
-
-      let actual: any;
-      const app = createApp().get(
-        "/api/users/**:id",
-        (ctx) => void (actual = ctx),
-      );
-      await createTestAppClient(app).fetch("GET", "/api/users/**:id", {
-        params: { id },
-      });
-
-      expect(actual.path).toEqual(expectedPath);
-      expect(actual.params).toEqual(expectedParams);
     });
   });
 
